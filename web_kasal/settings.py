c9c9e374c470e3eb1498130e1c216f0f611aa7b7
lazailemurillo <lazailemurillon@mabinicolleges.edu.ph>
2026-09-17 04:45:03 +0800

    crsf2

--- web_kasal/settings.py
+++ web_kasal/settings.py
@@ -30,8 +30,8 @@ SECRET_KEY = 'django-insecure-(8o7tqs5*85dm=0najxv3%t791d@*tllj@#e$b3clg@*3oap&z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*",
-                 "web-kasal-production.up.railway.app",]
+ALLOWED_HOSTS = ["*"]
+
 CSRF_TRUSTED_ORIGINS = [
     "https://web-kasal-production.up.railway.app",
 ]
